3c37b22cba01711f9fc92cc4931dfa63c5365e7e
Luana Martins <luamartinstravel@gmail.com>
2024-07-21 15:05:22 -0300

    feat: add navigation to home on button click in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,11 @@
+import { useNavigate } from 'react-router-dom';
+
 function Header() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate('/');
+  };
 
   return (
     <div className="bg_blue-custom h-screen">   
@@ -8,17 +15,17 @@ function Header() {
         <ul className="flex gap-6 mr-60">
           <li>
             <a href="#">
-              <img className="size-6" src="./instagram.svg" alt="" />
+              <img className="size-6" src="./instagram.svg" alt="Instagram" />
             </a>
           </li>
           <li>
             <a href="#">
-              <img className="size-6" src="./youtube.svg" alt="" />
+              <img className="size-6" src="./youtube.svg" alt="YouTube" />
             </a>
           </li>
           <li>
             <a href="#">
-              <img className="size-6" src="./pinterest.svg" alt="" />
+              <img className="size-6" src="./pinterest.svg" alt="Pinterest" />
             </a>
           </li>
         </ul>
@@ -27,17 +34,20 @@ function Header() {
       <div className="mt-36 p-6 ml-56 flex justify-between mr-60">
         <div>
           <h1 className="text-7xl leading-16 ">Bem vindos ao <br></br> <span className="text-pink-500">meu</span> blog ♡</h1>
-          <button className="rounded-2xl bg-slate-50 px-3.5 py-2.5 text-lg font-semibold text-black shadow-sm hover:bg-slate-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-10 w-36 h-12">
+          <button 
+            onClick={handleButtonClick}
+            className="rounded-2xl bg-slate-50 px-3.5 py-2.5 text-lg font-semibold text-black shadow-sm hover:bg-slate-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-10 w-36 h-12"
+          >
             receitas
           </button>
         </div>
 
         <div>
-          <img className="" src="./hero_ghost.png" alt="" />
+          <img className="" src="./hero_ghost.png" alt="Hero" />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/Bolsas.tsx
+++ src/pages/Bolsas.tsx
@@ -1,8 +1,43 @@
+import Item from "../components/Item";
+
 export default function Bolsas() {
-
   return (
-    <></>
-  )
+    <div className="p-10">
+      <div className="flex items-center justify-between">
+        <img className="size-50 p-6 ml-56" src="./logo.png" alt="luuas crochet" />
+
+        <ul className="flex gap-6 mr-60">
+          <li>
+            <a href="#">
+              <img className="size-6" src="./instagram.svg" alt="Instagram" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img className="size-6" src="./youtube.svg" alt="YouTube" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img className="size-6" src="./pinterest.svg" alt="Pinterest" />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h1 className="text-center text-3xl mb-8">Bolsas</h1>
+
+        <ul className="flex gap-4 justify-center">
+          <Item link="#" imageSrc="./bags.png" altText="bags crochet" />
+          <Item link="#" imageSrc="./bags.png" altText="bags crochet" />
+          <Item link="#" imageSrc="./bags.png" altText="bags crochet" />
+        </ul>
+
+        <div className="flex justify-center mt-4">
+          <img className="size-5 cursor-pointer" src="./arrow-right.svg" alt="arrow right" />
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
